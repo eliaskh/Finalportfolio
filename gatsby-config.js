e12d7082366834a11e83029caa0f83dc389ba00a
elias <eliaskhs@hotmail.com>
2022-05-12 16:26:42 +0300

    second

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   flags: {
-     PRESERVE_WEBPACK_CACHE: false,
-     PRESERVE_FILE_DOWNLOAD_CACHE: false
-   },
+    PRESERVE_WEBPACK_CACHE: false,
+    PRESERVE_FILE_DOWNLOAD_CACHE: false,
+  },
   siteMetadata: {
     title: `Spet - Gatsby React IT & Business Startup`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `https://upwork-her.herokuapp.com`,
+        apiURL: `https://portfoliogreek.herokuapp.com`,
         queryLimit: 1000,
         contentTypes: [
           "top-features",
